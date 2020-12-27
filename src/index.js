ed5fca40eb2ffc6addc1cbeb57a9bed5b35bc80d
hcjamm@gmail.com <hcjamm@gmail.com>
2020-12-27 23:35:18 +0800

    1

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/main.scss';
-import MDWA from './components/MDWA';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<MDWA />, document.getElementById('root'));
-registerServiceWorker();
